684e4e75eeb2718cef3680c3bc5b6f66a75d34c9
Mohammed Jaafar <mohdahmedjafar@gmail.com>
2023-10-01 22:20:40 +0100

    AirBnB clone - RESTful API

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -7,7 +7,7 @@ distributes an archive to the web servers
 from fabric.api import env, local, put, run
 from datetime import datetime
 from os.path import exists, isdir
-env.hosts = ['142.44.167.228', '144.217.246.195']
+env.hosts = ['54.87.181.20', '18.234.129.115']
 
 
 def do_pack():
