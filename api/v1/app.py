93bee24dd9491ff645809f52fd4acbc8ea71b00d
Mohammed Jaafar <mohdahmedjafar@gmail.com>
2023-10-02 03:01:36 +0100

    AirBnB clone - RESTful API

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,29 +7,35 @@ from flask import Flask, jsonify
 from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
-from flasgger import Swagger
-from flasgger.utils import swag_from
+#from flasgger import Swagger
+#from flasgger.utils import swag_from
 
 app = Flask(__name__)
+
 app.register_blueprint(app_views)
-cors = CORS(app, resources={r"/api/v1/*": {"origins": "0.0.0.0"}})
+app.url_map.strict_slashes = False
+
+#cors = CORS(app, resources={r"/api/v1/*": {"origins": "0.0.0.0"}})
 
 @app.teardown_appcontext
-def teardown(self):
+#def teardown(self):
+def teardown_engine(exception):
     """Removes the current SQLAlchemy Session"""
     return storage.close()
 
 @app.errorhandler(404)
-def error(e):
+#def error(e):
+def not_found(error):
     """Handler for 404 errors"""
-    return jsonify({"error": "Not found"}), 404
+    response = {"error": "Not found"}
+    return jsonify(response), 404
 
-app.config['SWAGGER'] = {
-    'title': 'AirBnB clone Restful API',
-    'uiversion': 3
-}
+# app.config['SWAGGER'] = {
+#     'title': 'AirBnB clone Restful API',
+#     'uiversion': 3
+# }
 
-Swagger(app)
+#Swagger(app)
 
 if __name__ == '__main__':
     host = getenv("HBNB_API_HOST") if getenv("HBNB_API_HOST") else "0.0.0.0"
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,27 +2,42 @@
 """This module implement a rule that returns the status of the application Done"""
 from flask import jsonify
 import models
+from models import storage #added 
 from api.v1.views import app_views
-from models.amenity import Amenity
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
-from models.user import User
+# from models.amenity import Amenity
+# from models.city import City
+# from models.place import Place
+# from models.review import Review
+# from models.state import State
+# from models.user import User
 
 @app_views.route("/status", strict_slashes=False)
 def view_status():
     """View function that return a json message"""
-    return jsonify({"status": "OK"})
+    response = {"status": "OK"}
+    return jsonify(response)
+
+# @app_views.route("/stats", strict_slashes=False)
+# def view_stats():
+#     """Veiw function that retrieves the number of each object by type"""
+#     return jsonify({
+#         "amenities": models.storage.count(Amenity),
+#         "cities": models.storage.count(City),
+#         "places": models.storage.count(Place),
+#         "reviews": models.storage.count(Review),
+#         "states": models.storage.count(State),
+#         "users": models.storage.count(User)
+#     })
 
 @app_views.route("/stats", strict_slashes=False)
 def view_stats():
-    """Veiw function that retrieves the number of each object by type"""
-    return jsonify({
-        "amenities": models.storage.count(Amenity),
-        "cities": models.storage.count(City),
-        "places": models.storage.count(Place),
-        "reviews": models.storage.count(Review),
-        "states": models.storage.count(State),
-        "users": models.storage.count(User)
-    })
+    """View function that retrieves the number of each object by type."""
+    stats = {
+        "amenities": models.storage.count('Amenity'),
+        "cities": models.storage.count('City'),
+        "places": models.storage.count('Place'),
+        "reviews": models.storage.count('Review'),
+        "states": models.storage.count('State'),
+        "users": models.storage.count(models.user.User)
+    }
+    return jsonify(stats)
