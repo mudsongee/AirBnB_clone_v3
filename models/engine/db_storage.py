5d32d476c28297ac603f2874c7bf19c51e6ac175
Mohammed Jaafar <mohdahmedjafar@gmail.com>
2023-10-02 16:28:52 +0100

    AirBnB clone - RESTful API

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -81,7 +81,7 @@ class DBStorage:
         if cls in classes.values() and type(id) == str:
             d_obj = self.all(cls)
             for key, value in d_obj.items():
-                if key.split("-")[1] == id:
+                if key.split("")[1] == id:
                     return value
         return None
     # def get(self, cls, id):
