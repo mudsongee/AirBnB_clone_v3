c31220a4743e535a6473ac1159188dfda376c70e
Mohammed Jaafar <mohdahmedjafar@gmail.com>
2023-10-03 00:41:46 +0100

    AirBnB clone - RESTful API

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -8,7 +8,8 @@ from models.review import Review
 from models.user import User
 
 
-@app_views.route("/places/<place_id>/reviews", methods=['GET'], strict_slashes=False)
+@app_views.route('/places/<place_id>/reviews', methods=['GET'],
+                 strict_slashes=False)
 def get_reviews(place_id):
     """Get list of all Review objects of a Place"""
     place = storage.get(Place, place_id)
@@ -27,7 +28,8 @@ def get_review(review_id):
     return jsonify(review.to_dict())
 
 
-@app_views.route("/reviews/<review_id>", methods=['DELETE'], strict_slashes=False)
+@app_views.route('/reviews/<review_id>', methods=['DELETE'],
+                 strict_slashes=False)
 def delete_review(review_id):
     """Delete a Review object by its ID"""
     review = storage.get(Review, review_id)
@@ -67,7 +69,8 @@ def create_review(place_id):
     return jsonify(new_review.to_dict()), 201
 
 
-@app_views.route("/reviews/<review_id>", methods=['PUT'], strict_slashes=False)
+@app_views.route('/reviews/<review_id>', methods=['PUT'],
+                 strict_slashes=False)
 def update_review(review_id):
     """Update a Review object by its ID"""
     review = storage.get(Review, review_id)
