7ac30e612ab76c2113596ee852607886f32defef
Mohammed Jaafar <mohdahmedjafar@gmail.com>
2023-10-02 17:11:41 +0100

    AirBnB clone - RESTful API

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-"""
-Contains the class DBStorage
-"""
-
+"""Contains the class DBStorage"""
 import models
 from models.amenity import Amenity
 from models.base_model import BaseModel, Base
@@ -15,6 +12,7 @@ from os import getenv
 import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
+
 
 classes = {"Amenity": Amenity, "City": City,
            "Place": Place, "Review": Review, "State": State, "User": User}
