129c31f17b89654c151cbe4fc25f179f56b5016e
Mohammed Jaafar <mohdahmedjafar@gmail.com>
2023-10-02 20:22:51 +0100

    AirBnB clone - RESTful API

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -53,6 +53,7 @@ def post_state():
     new_state.save()
     return jsonify(new_state.to_dict()), 201
 
+
 @app_views.route('/states/<path:state_id>', methods=['PUT'],
                  strict_slashes=False)
 @swag_from('documentation/state/put_state.yml', methods=['PUT'])
