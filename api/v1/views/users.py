e7c553a33f80df27917434cf35d95bcb1b964ece
Mohammed Jaafar <mohdahmedjafar@gmail.com>
2023-10-02 23:04:48 +0100

    AirBnB clone - RESTful API

---
+++ api/v1/views/users.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+"""Creates a view for User objects - handles all default RESTUL API actions"""
+from flask import Flask, jsonify, request, abort
+from models import storage
+from models.user import User
+from api.v1.views import app_views
+
+@app_views.route('/users', methods=['GET'], strict_slashes=False)
+def get_users():
+    """Retrieves the list of all User objects"""
+    users = [user.to_dict() for user in storage.all(User).values()]
+    return jsonify(users)
+
+@app_views.route('/users/<user_id>', methods=['GET'], strict_slashes=False)
+def get_user(user_id):
+    """Retrieves a User object by ID"""
+    user = storage.get(User, user_id)
+    if user:
+        return jsonify(user.to_dict())
+    abort(404)
+
+@app_views.route('/users/<user_id>', methods=['DELETE'], strict_slashes=False)
+def delete_user(user_id):
+    """Deletes a User object by ID"""
+    user = storage.get(User, user_id)
+    if user:
+        storage.delete(user)
+        storage.save()
+        return jsonify({}), 200
+    abort(404)
+
+@app_views.route('/users', methods=['POST'], strict_slashes=False)
+def create_user():
+    """Creates a new User object"""
+    data = request.get_json()
+    if not data:
+        abort(400, 'Not a JSON')
+    if 'email' not in data:
+        abort(400, 'Missing email')
+    if 'password' not in data:
+        abort(400, 'Missing password')
+    new_user = User(**data)
+    new_user.save()
+    return jsonify(new_user.to_dict()), 201
+
+@app_views.route('/users/<user_id>', methods=['PUT'], strict_slashes=False)
+def update_user(user_id):
+    """Updates a User object by ID"""
+    user = storage.get(User, user_id)
+    if user:
+        data = request.get_json()
+        if not data:
+            abort(400, 'Not a JSON')
+        for key, value in data.items():
+            if key not in ['id', 'email', 'created_at', 'updated_at']:
+                setattr(user, key, value)
+        user.save()
+        return jsonify(user.to_dict()), 200
+    abort(404)
