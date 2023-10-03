4b8aa4dd3d236f0158cce59ec30f7dd78e226106
Mohammed Jaafar <mohdahmedjafar@gmail.com>
2023-10-03 01:26:40 +0100

    AirBnB clone - RESTful API

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -8,8 +8,9 @@ from models.user import User
 from api.v1.views import app_views
 from flasgger import swag_from
 
-@app_views.route('/cities/<city_id>/places', methods=['GET'], strict_slashes=False)
-@swag_from('documetation/place/get_place.yml')  # Replace with your Swagger YAML file path
+@app_views.route('/cities/<city_id>/places', methods=['GET'],
+                 strict_slashes=False)
+@swag_from('documetation/place/get_place.yml')
 def get_places_by_city(city_id):
     """Get list of all Place objects of a City"""
     city = storage.get(City, city_id)
@@ -19,8 +20,9 @@ def get_places_by_city(city_id):
     places = [place.to_dict() for place in city.places]
     return jsonify(places)
 
-@app_views.route('/places/<place_id>', methods=['GET'], strict_slashes=False)
-@swag_from('documetation/place/get_place.yml')  # Replace with your Swagger YAML file path
+@app_views.route('/places/<place_id>', methods=['GET'],
+                 strict_slashes=False)
+@swag_from('documetation/place/get_place.yml')
 def get_place(place_id):
     """Get a Place object by its ID"""
     place = storage.get(Place, place_id)
@@ -29,8 +31,10 @@ def get_place(place_id):
 
     return jsonify(place.to_dict())
 
-@app_views.route('/places/<place_id>', methods=['DELETE'], strict_slashes=False)
-@swag_from('documetation/place/delete_place.yml')  # Replace with your Swagger YAML file path
+
+@app_views.route('/places/<place_id>', methods=['DELETE'],
+                 strict_slashes=False)
+@swag_from('documetation/place/delete_place.yml')
 def delete_place(place_id):
     """Delete a Place object by its ID"""
     place = storage.get(Place, place_id)
@@ -41,8 +45,10 @@ def delete_place(place_id):
     storage.save()
     return jsonify({})
 
-@app_views.route('/cities/<city_id>/places', methods=['POST'], strict_slashes=False)
-@swag_from('documetation/place/create_place.yml')  # Replace with your Swagger YAML file path
+
+@app_views.route('/cities/<city_id>/places', methods=['POST'],
+                 strict_slashes=False)
+@swag_from('documetation/place/create_place.yml')
 def create_place(city_id):
     """Create a new Place object for a City"""
     city = storage.get(City, city_id)
@@ -70,7 +76,9 @@ def create_place(city_id):
     new_place.save()
     return jsonify(new_place.to_dict()), 201
 
-@app_views.route('/places/<place_id>', methods=['PUT'], strict_slashes=False)
+
+@app_views.route('/places/<place_id>', methods=['PUT'],
+                 strict_slashes=False)
 @swag_from('documetation/place/update_place.yml')  # Replace with your Swagger YAML file path
 def update_place(place_id):
     """Update a Place object by its ID"""
