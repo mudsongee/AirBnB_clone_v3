7cbfe3938d0efaa963c17aad8fe8c0989084f7d9
Mohammed Jaafar <mohdahmedjafar@gmail.com>
2023-10-03 20:39:29 +0100

    AirBnB clone - RESTful API

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,7 +3,7 @@
 This module implement a rule that returns
 the status of the application Done
 """
-from flask import jsonify, request
+from flask import jsonify
 import models
 from models import storage
 from api.v1.views import app_views
@@ -25,8 +25,6 @@ def view_status():
 @app_views.route("/stats", strict_slashes=False)
 def view_stats():
     """View function that retrieves the number of each object by type."""
-    if request.method == 'GET':
-        response = {}
     stats = {
         "amenities": models.storage.count('Amenity'),
         "cities": models.storage.count('City'),
@@ -34,8 +32,5 @@ def view_stats():
         "reviews": models.storage.count('Review'),
         "states": models.storage.count('State'),
         "users": models.storage.count('User'),
-
     }
-    for key, value in stats.items():
-            response[value] = storage.count(key)
     return jsonify(stats)
