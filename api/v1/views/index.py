222862d3fc0c25f6dee564ec0cb86a851eb77379
Mohammed Jaafar <mohdahmedjafar@gmail.com>
2023-10-04 03:10:51 +0100

    AirBnB clone - RESTful API

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -7,13 +7,13 @@ the status of the application Done
 from flask import jsonify
 from models import storage
 from api.v1.views import app_views
-# from models.amenity import Amenity
-# from models.city import City
-# from models.place import Place
-# from models.review import Review
-# from models.state import State
-# from models.user import User
 import models
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 @app_views.route("/status", strict_slashes=False)
